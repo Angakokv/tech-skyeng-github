b19ee4418065a811a88c9da3f86422d3e5099912
angakok <notifyevent@gmail.com>
2020-09-10 15:53:06 +0300

    1. Locator changed. 2. Added ui test execution video.

--- e2e/page-objects/product/product-page.po.ts
+++ e2e/page-objects/product/product-page.po.ts
@@ -6,7 +6,7 @@ export class ProductPage {
     static readonly productEditionCard = (editionName: string) =>
         element(By.cssContainingText("div[data-component=\"ProductCard\"]", editionName));
     static readonly wishButton = (editionName: string) =>
-        ProductPage.productEditionCard(editionName).element(By.css("button[class*=\"WishButton\"]"));
+        ProductPage.productEditionCard(editionName).element(By.css("button[data-testid=\"wish-button\"]"));
 
     static async clickWishButton(editionName: string) {
         const wishButton = this.wishButton(editionName);
